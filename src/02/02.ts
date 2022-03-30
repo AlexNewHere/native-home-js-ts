e6ed2cee1eab16a6f847308f2c96a6341c2bbc53
Alex <tendren3310@googlemail.com>
2022-03-30 18:01:38 +0300

    Lesson 2

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,3 +1,55 @@
-const student= {
-
+type LocalCityType = {
+    title: string
+    countryTitle: string
 }
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    name: string
+    age: number
+    id: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Alex',
+    age: 34,
+    isActive: false,
+    address: {
+        streetTitle: 'ponomareva',
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus'
+        },
+
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
+
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
